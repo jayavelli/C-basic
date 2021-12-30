bae366c853aa51e5b9a41e8e5aed41fe05d9829c
jayavelli <96563831+jayavelli@users.noreply.github.com>
2021-12-29 22:24:02 -0800

    Add files via upload

---
+++ jk.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+ int m,p,c,s ;
+ printf("enter m,p,c,s");
+ scanf("%d%d%d%d",&m,&p,&c,&s);
+  if(m>35&&p>35&&c>35&&s>35)
+  {
+  	printf("pass");
+  }
+   else;
+  {
+  	printf("fail");
+}
+
+
+
+
+
+}
+ 
+
+
+
+
+
+
+
